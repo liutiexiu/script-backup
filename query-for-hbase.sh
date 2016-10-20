6e350bfc99666700de05cf3781d97ce80a507ef4
liutiexiu <liulvnju@gmail.com>
2016-10-20 18:18:27 +0800

    Update query-for-hbase.sh

--- query-for-hbase.sh
+++ query-for-hbase.sh
@@ -1,9 +1,9 @@
 #query for hbase
 
-PATH='somepath'
+LOG_PATH='somepath'
 
 #find SocketTimeout of region
-grep "java.net.SocketTimeoutException: Call to c3-hadoop-srv-st" $PATH | grep CST | awk '{print $4" "$11}' | sed -e "s/:.. /:xx /g" | uniq -c
+grep "java.net.SocketTimeoutException: Call to c3-hadoop-srv-st" $LOG_PATH | grep CST | awk '{print $4" "$11}' | sed -e "s/:.. /:xx /g" | uniq -c
 
 #count slow method
-grep "SLOW METHOD" $PATH | awk -F "," '{print $1}' | awk -F ":" '{print $1":"$2}' | sed -e 's/.$/x/g' | uniq -c
+grep "SLOW METHOD" $LOG_PATH | awk -F "," '{print $1}' | awk -F ":" '{print $1":"$2}' | sed -e 's/.$/x/g' | uniq -c
