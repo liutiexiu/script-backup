452327e37e1421bcca32aa9b43ce32b2d4393ad4
liutiexiu <liulvnju@gmail.com>
2016-10-20 20:18:03 +0800

    Update query-for-hbase.sh

--- query-for-hbase.sh
+++ query-for-hbase.sh
@@ -1,9 +1,19 @@
 #query for hbase
 
-LOG_PATH='somepath'
+MT_LOG_PATH='mtPath'
 
 #find SocketTimeout of region
-grep "java.net.SocketTimeoutException: Call to c3-hadoop-srv-st" $LOG_PATH | grep CST | awk '{print $4" "$11}' | sed -e "s/:.. /:xx /g" | uniq -c
+grep "java.net.SocketTimeoutException: Call to c3-hadoop-srv-st" $MT_LOG_PATH | grep CST | awk '{print $4" "$11}' | sed -e "s/:.. /:xx /g" | uniq -c
 
 #count slow method
-grep "SLOW METHOD" $LOG_PATH | awk -F "," '{print $1}' | awk -F ":" '{print $1":"$2}' | sed -e 's/.$/x/g' | uniq -c
+grep "SLOW METHOD" $MT_LOG_PATH | awk -F "," '{print $1}' | awk -F ":" '{print $1":"$2}' | sed -e 's/.$/x/g' | uniq -c
+
+
+
+NGINX_LOG_PATH='nginxPath'
+
+#count 500 in nginx
+cat $NGINX_LOG_PATH | awk -F "\t" '{print $4"\tcode="$18}' | grep "code=500" | awk -F ":" '{print $1":"$2":"$3}' | uniq -c
+
+#find 500 url
+cat $NGINX_LOG_PATH | awk -F "\t" '{print $4"\t"$1"\t"$5"\tcode="$18}' | grep "code=500"
