c34f9d094ae7f72e518b734abaaf334fda09f1c6
liutiexiu <liulvnju@gmail.com>
2016-12-07 15:42:59 +0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,6 +2,14 @@
 ## some unknown bash ##
 #######################
 
+#display git branch
+function git_branch {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return;
+  echo "("${ref#refs/heads/}") ";
+}
+PROMPT_COMMAND='echo -ne "\033]0;${HOSTNAME}\007"'
+PS1="[\[\033[1;32m\]\w\[\033[0m\]] \[\033[1;35m\]\$(git_branch)\[\033[0m\]$ "
+
 #get specific line from text file
 head -n 10 fileName | tail -n 1
 awk 'NR==10' fileName
